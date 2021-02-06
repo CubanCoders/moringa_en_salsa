4771252b5b47a646200fa40c148cbc99bb1a4d1e
adrianhdez929 <adrianhdez929@gmail.com>
2021-02-06 01:37:33 -0500

    fixed structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './client/index.css';
-import App from './client/App';
-import reportWebVitals from './client/reportWebVitals';
+import './styles/index.css';
+import App from './views/App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
